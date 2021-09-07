2fc8c50ad7a101b12433b6dfab0ccb3cd685fee2
VitorHugoSantos <vitinho.bs03@gmail.com>
2021-09-07 17:29:21 -0300

    Pagina Login

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <div class="contentLogin">
+            <p>AAaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login'
+    };
+</script>
+
+<style lang="scss">
+    .login{
+        justify-content: center;
+        .contentLogin{
+            width: 50%;
+            border: 1px solid black;
+        }
+    }
+</style>
